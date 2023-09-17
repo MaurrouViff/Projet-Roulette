ad70e95c7a7a387527e8aef6a456712eda0b7fb5
Aurélien <rofldu08@gmail.com>
2023-09-17 18:52:58 +0200

    bdd du projet

--- db/Roulette.sql
+++ db/Roulette.sql
@@ -2,19 +2,23 @@ DROP DATABASE IF EXISTS Roulette;
 CREATE DATABASE Roulette;
 USE Roulette;
 
-CREATE TABLE eleves (
+CREATE TABLE eleve (
                         id INT AUTO_INCREMENT PRIMARY KEY,
-                        nom VARCHAR(255) NOT NULL
+                        classe VARCHAR(255) NOT NULL,
+                        nomfamille VARCHAR(255) NOT NULL,
+                        prenom VARCHAR(255) NOT NULL
 );
 
-INSERT INTO eleves (nom) VALUES
-                             ('Élève 1'),
-                             ('Élève 2'),
-                             ('Élève 3'),
-                             ('Élève 4'),
-                             ('Élève 5'),
-                             ('Élève 6'),
-                             ('Élève 7'),
-                             ('Élève 8'),
-                             ('Élève 9'),
-                             ('Élève 10');
+INSERT INTO eleve (id, classe, nomfamille, prenom) VALUES
+                             (1, 'SIO2', 'AUBRIET', 'Aurélien'),
+                             (2, 'SIO2', 'BARIAL', 'Benjamin'),
+                             (3, 'SIO2', 'GUILLAUME', 'Corentin'),
+                             (4, 'SIO2', 'BON', 'Jean'),
+                             (5, 'SIO2', 'NEYMAR', 'Jean'),
+                             (6, 'SIO2', 'DE LANGE', 'Aymeric'),
+                             (7, 'SIO2', 'Gadroy', 'Léo'),
+                             (8, 'SIO2', 'TURQUIER', 'Victor'),
+                             (9, 'SIO2', 'LHERME', 'Hugo'),
+                             (10, 'SIO2', 'CORDIER', 'Eugène'),
+                             (11, 'SIO2', 'NOËL', 'Père')
+
