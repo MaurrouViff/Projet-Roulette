cf5911f7e52e020a4c57bde7dac5f2e9329367c8
Aurélien <rofldu08@gmail.com>
2023-09-18 08:41:31 +0200

    bdd du projet roulette

--- db/Roulette.sql
+++ db/Roulette.sql
@@ -6,19 +6,21 @@ CREATE TABLE eleve (
                         id INT AUTO_INCREMENT PRIMARY KEY,
                         classe VARCHAR(255) NOT NULL,
                         nomfamille VARCHAR(255) NOT NULL,
-                        prenom VARCHAR(255) NOT NULL
+                        prenom VARCHAR(255) NOT NULL,
+                        note INT NOT NULL,
+                        passage VARCHAR(255) NOT NULL
 );
 
-INSERT INTO eleve (id, classe, nomfamille, prenom) VALUES
-                             (1, 'SIO2', 'AUBRIET', 'Aurélien'),
-                             (2, 'SIO2', 'BARIAL', 'Benjamin'),
-                             (3, 'SIO2', 'GUILLAUME', 'Corentin'),
-                             (4, 'SIO2', 'BON', 'Jean'),
-                             (5, 'SIO2', 'NEYMAR', 'Jean'),
-                             (6, 'SIO2', 'DE LANGE', 'Aymeric'),
-                             (7, 'SIO2', 'Gadroy', 'Léo'),
-                             (8, 'SIO2', 'TURQUIER', 'Victor'),
-                             (9, 'SIO2', 'LHERME', 'Hugo'),
-                             (10, 'SIO2', 'CORDIER', 'Eugène'),
-                             (11, 'SIO2', 'NOËL', 'Père')
+INSERT INTO eleve (id, classe, nomfamille, prenom, note, passage) VALUES
+                             (1, 'SIO2', 'AUBRIET', 'Aurélien', 0, 'non'),
+                             (2, 'SIO2', 'BARIAL', 'Benjamin', 0, 'non'),
+                             (3, 'SIO2', 'GUILLAUME', 'Corentin', 0, 'non'),
+                             (4, 'SIO2', 'BON', 'Jean', 0, 'non'),
+                             (5, 'SIO2', 'NEYMAR', 'Jean', 0, 'non'),
+                             (6, 'SIO2', 'DE LANGE', 'Aymeric', 0, 'non'),
+                             (7, 'SIO2', 'Gadroy', 'Léo', 0, 'non'),
+                             (8, 'SIO2', 'TURQUIER', 'Victor', 0, 'non'),
+                             (9, 'SIO2', 'LHERME', 'Hugo', 0, 'non'),
+                             (10, 'SIO2', 'CORDIER', 'Eugène', 0, 'non'),
+                             (11, 'SIO2', 'NOËL', 'Père', 0, 'non')
 
