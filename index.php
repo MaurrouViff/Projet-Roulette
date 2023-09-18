a3c814922a954fb58e9054b53ae198bf3ef5d283
Aurélien <rofldu08@gmail.com>
2023-09-18 08:42:12 +0200

    index du projet roulette

--- index.php
+++ index.php
@@ -3,20 +3,24 @@ include_once("modele/bd.eleve.inc.php");
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
+$eleveDb = new EleveDatabase();
+
 // Affiche la liste des élèves
 try {
-    $listeEleve = getEleve();
+    $listeEleve = $eleveDb->getEleve();
 } catch (PDOException $e) {
     // Gestion de l'erreur de base de données
     echo "Erreur de base de données : " . $e->getMessage();
     die();
 }
+
 // Permet de choisir un élève
 $eleveChoisi = null;
 
 if (isset($_POST['select-student'])) {
     try {
-        $eleve = selectRandomEleve();
+        $eleve = $eleveDb->selectRandomEleve();
         if ($eleve) {
             $eleveChoisi = $eleve[0];
         }
@@ -24,6 +28,23 @@ if (isset($_POST['select-student'])) {
         // Gestion de l'erreur de base de données
         echo "Erreur de base de données : " . $e->getMessage();
         die();
+    }
+}
+
+// Permet de supprimer un élève de la base de données
+if (isset($_GET['action']) && $_GET['action'] === 'supprimer' && isset($_GET['id'])) {
+    $idASupprimer = $_GET['id'];
+    $eleveExiste = $eleveDb->checkIfStudentExists($idASupprimer);
+    if ($eleveExiste) {
+        $messageSuppression = $eleveDb->deleteStudentById($idASupprimer);
+        if ($messageSuppression === "Suppression réussie") {
+            header("Location: index.php");
+            $eleveChoisi = null;
+        } else {
+            echo $messageSuppression;
+        }
+    } else {
+        echo "Élève introuvable dans la base de données.";
     }
 }
 ?>
@@ -43,32 +64,33 @@ if (isset($_POST['select-student'])) {
 
 <div class="container">
     <section class="section">
-    <h1 class="red">Voici la liste des élèves :</h1>
+        <h1 class="red">Voici la liste des élèves :</h1>
 
-    <table>
-        <thead>
-        <tr>
-            <th>Nom</th>
-            <th>Prénom</th>
-            <th>Classe</th>
-            <th>ID</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($listeEleve as $eleve) { ?>
+        <table>
+            <thead>
             <tr>
-                <td><?= htmlspecialchars($eleve["nomfamille"]) ?></td>
-                <td><?= htmlspecialchars($eleve["prenom"]) ?></td>
-                <td><?= htmlspecialchars($eleve["classe"]) ?></td>
-                <td><?= $eleve["id"] ?></td>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Classe</th>
+                <th>ID</th>
             </tr>
-        <?php } ?>
-        </tbody>
-    </table>
-    <hr>
+            </thead>
+            <tbody>
+            <?php foreach ($listeEleve as $eleve) { ?>
+                <tr>
+                    <td><?= htmlspecialchars($eleve["nomfamille"]) ?></td>
+                    <td><?= htmlspecialchars($eleve["prenom"]) ?></td>
+                    <td><?= htmlspecialchars($eleve["classe"]) ?></td>
+                    <td><?= $eleve["id"] ?></td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+        <hr>
         <h3 class="red">Elève choisi : </h3>
         <?php if ($eleveChoisi) { ?>
             <p class="orange">Nom : <?= htmlspecialchars($eleveChoisi["nomfamille"]) ?>, Prénom : <?= htmlspecialchars($eleveChoisi["prenom"]) ?>, Classe : <?= htmlspecialchars($eleveChoisi["classe"]) ?>, ID : <?= $eleveChoisi["id"] ?></p>
+            <a href="?action=supprimer&id=<?= $eleveChoisi["id"] ?>" onclick="return confirm('Êtes-vous sûr de vouloir supprimer cet élève ?')">Supprimer</a>
         <?php } else { ?>
             <p class="orange">Aucun élève choisi pour le moment.</p>
         <?php } ?>
@@ -76,7 +98,7 @@ if (isset($_POST['select-student'])) {
         <form method="post">
             <button type="submit" class="first-button" name="select-student">Sélectionner un élève</button>
         </form>
-    <hr>
+        <hr>
     </section>
 </div>
 
