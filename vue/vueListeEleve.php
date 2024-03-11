64d61f6582a77e77ff849dd3c761966ed8233dc1
MaurrouViff <rofldu08@gmail.com>
2024-03-11 09:39:54 +0100

    Projet roulette recodé en MVC

--- vue/vueListeEleve.php
+++ vue/vueListeEleve.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php echo $titre; ?></title>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <style>
+        @import url("css/style.css");
+    </style>
     <link rel="icon" href="assets/icon/roulette-svg.png"/>
 </head>
 <body>
