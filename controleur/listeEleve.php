67536857952cdba2274a7e0e45b34e2adf1f544b
MaurrouViff <rofldu08@gmail.com>
2024-04-02 20:17:27 +0200

    Projet roulette recodé en MVC

--- controleur/listeEleve.php
+++ controleur/listeEleve.php
@@ -183,7 +183,6 @@ if (isset($_POST['add-value'])) {
     }
 }
 
-
 // appel du script de vue qui permet de gerer l'affichage des donnees
 $titre = "Projet Roulette";
 include "$racine/vue/vueListeEleve.php";
